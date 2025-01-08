f850b9299a4d63206cd24e53d5431f41ee29b91e
bhenry3145 <bhenry3145@gmail.com>
2025-01-08 15:43:43 -0800

    fixed madlib

--- Services/MadLibServices.cs
+++ Services/MadLibServices.cs
@@ -11,7 +11,7 @@ namespace FiveToSeven.Services
 
         public string FormMadLib(string place, string adjective, string animal, string adverb, string presentTenseVerb, string humanName, string amount, string itemPlural, string vehicle, string country)
         {
-            madLib = "One day you wake up at " + place + " and you see a very " + adjective + " " + animal + "." + " The " + animal + " is " + adverb + " " + presentTenseVerb + " right next to you. Unfortunately you " + humanName + ", is terrified of the " + animal + "." + "All you have are " + amount + itemPlural + " in your pocket. Luckily for you a giant " + vehicle + " arrives " + "and you are taken all the way to " + country + ", where you are no longer afraid.";
+            madLib = "One day you wake up at " + place + " and you see a very " + adjective + " " + animal + "." + " The " + animal + " is " + adverb + " " + presentTenseVerb + " right next to you. Unfortunately you " + humanName + ", is terrified of the " + animal + "." + " All you have are " + amount + " " + itemPlural + " in your pocket. Luckily for you a giant " + vehicle + " arrives " + "and you are taken all the way to " + country + ", where you are no longer afraid.";
             return madLib;
         }
     }
